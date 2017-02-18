1572fbeac38e7cd3b790f03cfeafddbfbf822922
brudin <brudin@users.noreply.github.com>
2017-02-17 19:29:22 -0500

    Updated MCP mappings - be sure to run 'gradlew setupDecompWorkspace idea'

--- src/main/java/net/halalaboos/mcwrapper/impl/mixin/world/MixinWorld.java
+++ src/main/java/net/halalaboos/mcwrapper/impl/mixin/world/MixinWorld.java
@@ -1,7 +1,18 @@
 package net.halalaboos.mcwrapper.impl.mixin.world;
 
+import net.halalaboos.mcwrapper.api.util.Vector3i;
 import net.halalaboos.mcwrapper.api.world.World;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(net.minecraft.world.World.class) public abstract class MixinWorld implements World {
+
+	@Shadow
+	public abstract boolean setBlockToAir(BlockPos pos);
+
+	@Override
+	public void setToAir(Vector3i pos) {
+		setBlockToAir(new BlockPos(pos.x, pos.y, pos.z));
+	}
 }
