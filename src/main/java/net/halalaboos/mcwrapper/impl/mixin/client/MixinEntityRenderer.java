9b6af7134f04478aff4b2b580ae13f9f82a618e7
brudin <brudin@users.noreply.github.com>
2017-03-19 13:25:38 -0400

    Removed GuiIngame mixin, using EntityRenderer to dispatch the hud event.

--- src/main/java/net/halalaboos/mcwrapper/impl/mixin/client/MixinEntityRenderer.java
+++ src/main/java/net/halalaboos/mcwrapper/impl/mixin/client/MixinEntityRenderer.java
@@ -1,22 +1,20 @@
-package net.halalaboos.mcwrapper.impl.mixin.client.gui;
+package net.halalaboos.mcwrapper.impl.mixin.client;
 
 import net.halalaboos.mcwrapper.api.MCWrapper;
 import net.halalaboos.mcwrapper.api.event.render.HUDRenderEvent;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.GuiIngameForge;
+import net.minecraft.client.renderer.EntityRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-/**
- * NOTE: For non-forge implementations this needs to be pointed to Minecraft's GuiIngame class.
- */
-@Mixin(GuiIngameForge.class)
-public class MixinGuiIngame {
+@Mixin(EntityRenderer.class)
+public class MixinEntityRenderer {
 
-	@Inject(method = "renderGameOverlay", at = @At(value = "RETURN"))
-	public void renderGameOverlay(float partialTicks, CallbackInfo ci) {
+	@Inject(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiIngame;renderGameOverlay(F)V", shift = At.Shift.AFTER))
+	public void dispatchHUDEvent(float partialTicks, long nano, CallbackInfo ci) {
 		MCWrapper.getEventManager().publish(new HUDRenderEvent(Minecraft.getMinecraft().gameSettings.showDebugInfo, partialTicks));
 	}
+
 }
